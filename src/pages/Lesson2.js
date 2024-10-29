e7327a53dd7d56bc5f38c9df4009b28c99f304ba
patch <patchen21@gmail.com>
2024-10-28 22:06:08 -0400

    lesson2

--- src/pages/Lesson2.js
+++ src/pages/Lesson2.js
@@ -1,47 +1,85 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
 
 function Lesson2() {
-  useEffect(() => {
-    const hash = window.location.hash;
-    if (hash) {
-      const element = document.getElementById(hash.substring(1));
-      if (element) {
-        element.scrollIntoView({ behavior: "smooth", block: "start" });
-      }
-    }
-  }, []);
+  useSmoothScrollToSection();
 
   return (
     <div>
-      <h2>Lesson 2: </h2>
+      <h2>Lesson 2: かいもの (Shopping)</h2>
       <section id="section1">
-        <h3>Section 1</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore   et dolore magna aliqua. Ut enim
-          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
+        <h3>2.1: これ / それ / あれ / どれ</h3>
+        <p>これ = this</p>
+        <p>それ = that</p>
+        <p>あれ = that one over there</p>
+        <p>どれ = which one</p>
       </section>
 
       <section id="section2">
-        <h3>Section 2</h3>
+        <h3>2.2: この / その / あの / どの + Noun</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore   et dolore magna aliqua. Ut enim
-          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          Unlike the previous section, <em>kono</em>, <em>sono</em>,{" "}
+          <em>ano</em>, and dono has to be placed before a noun and cannot stand
+          alone.
+        </p>
+        <p>この = this thing</p>
+        <p>その = that thing</p>
+        <p>あの = that thing over there</p>
+        <p>どの = which thing</p>
+        <p>
+          Can be used in reference to people, unlike <em>kore</em>,{" "}
+          <em>sore</em>, and <em>are</em>, which is considered impolite.
         </p>
       </section>
 
       <section id="section3">
-        <h3>Section 3</h3>
+        <h3>2.3: ここ / そこ / あそこ / どこ</h3>
+        <p>Words for places</p>
+        <p>ここ = here, near me</p>
+        <p>そこ = there, near you</p>
+        <p>あそこ = over there</p>
+        <p>どこ = where</p>
+      </section>
+
+      <section id="section4">
+        <h3>2.4: だれの Noun</h3>
+        <p>How to ask who something belongs to</p>
+        <p>だれ = Who</p>
+        <p>だれの = Whose (with the posessive term <em>no</em>)</p>
+        <p>Example:</p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore   et dolore magna aliqua. Ut enim
-          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          これは だれの かばんですか <br />
+          Kore wa dare no kaban desu ka.
         </p>
+      </section>
+
+      <section id="section5">
+        <h3>2.5: Noun も</h3>
+        <p>The particle for the word "too"</p>
+        <p>
+          In Japanese, you can only put <em>mo</em> directly after the item.
+        </p>
+        <p>A は X です。 (A is X.)</p>
+        <p>B も X です。 (B too is X.)</p>
+      </section>
+
+      <section id="section6">
+        <h3>2.5: Noun じゃないです</h3>
+        <p>
+          To negate a statement, you replace <em>desu</em> with{" "}
+          <em>ja nai desu</em>
+        </p>
+        <p>A は X じゃないです。 (A is not X.)</p>
+        <p>
+          <em>Ja nai desu</em> is colloquial (used in ordinary or familiar
+          conversations).
+        </p>
+        <p>
+          じゃ is a contraction of <em>de wa</em>, so you might find the more
+          formal terms in the following:
+        </p>
+        <p>A は X じゃありません。 (more formal speech style)</p>
+        <p>A は X でわありません。 (formal for writing)</p>
       </section>
     </div>
   );
