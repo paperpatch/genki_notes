1215d7d38b5251b252f8463fc85ee1ab72114801
patch <patchen21@gmail.com>
2024-10-31 15:46:40 -0400

    lesson4

--- src/pages/Lesson4.js
+++ src/pages/Lesson4.js
@@ -1,8 +1,74 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson4() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 4</h2>
-      <p>Content 4 stuff</p>
+      <h2>Lesson 4: 初めてのデート (The First Date)</h2>
+      <section id="section1">
+        <h3>4.1: X があります / います</h3>
+        <div className="indent">
+          <p>があります / います means "there is / are X".</p>
+          <p>Use があります for non-living things.</p>
+          <p>Use います for living things.</p>
+          <p>It's accompanied by the particle が, isntead of は.e</p>
+          <p className="tab">
+            テレビがありません。
+            <br />I don't have a TV.
+          </p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>4.2: Describing Where Things Are</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>4.3: Past Tense of です</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>4.4: Past Tense of Verbs</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>4.5: も</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>4.6: 一時間</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section7">
+        <h3>4.7: たくさん</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section8">
+        <h3>4.7: と</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
