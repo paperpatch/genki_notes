869f1a831124f1138a723a7fd6a731bdca4b8130
patch <patchen21@gmail.com>
2024-11-09 12:46:43 -0500

    lesson5

--- src/pages/Lesson5.js
+++ src/pages/Lesson5.js
@@ -1,8 +1,53 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson5() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 5</h2>
-      <p>Content 5 stuff</p>
+      <h2>Lesson 5: 沖縄旅行 (A Trip to Okinawa)</h2>
+      <section id="section1">
+        <h3>5.1: Adjectives (Present Tense)</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>5.2: Adjectives (Past Tense)</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>5.3: Adjectives (Noun Modification)</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>5.4: 好き (な) / きらい (な)</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>5.5: ~ましょう / ~ましょうか</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>5.6: Counting</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
