46f620eeecc843797c85e0a13eaee90f77f480f5
patch <patchen21@gmail.com>
2024-11-11 14:00:41 -0500

    lesson6

--- src/pages/Lesson6.js
+++ src/pages/Lesson6.js
@@ -1,8 +1,55 @@
 function Lesson6() {
   return (
     <div>
-      <h2>Lesson 6</h2>
-      <p>Content 6 stuff</p>
+      <h2>Lesson 6: ロバートさんの一日 A Day in Robert's Life</h2>
+      <section id="section1">
+        <h3>6.1: Te-form</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>6.2: ~てください</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>6.3: Describing Two Activities</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>6.4: ~てもいいです</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>6.5: ~てはいけません</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>6.6: ~から</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section7">
+        <h3>6.7: ~ましょうか (Offering Assistance)</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
