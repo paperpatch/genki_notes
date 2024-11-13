0a730250eafd0e4bbc1b7e3391e3d8741b44906f
patch <patchen21@gmail.com>
2024-11-13 14:53:21 -0500

    lesson18

--- src/pages/Lesson18.js
+++ src/pages/Lesson18.js
@@ -1,8 +1,53 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson18() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 18</h2>
-      <p>Content 18 stuff</p>
+      <h2>Lesson 18: ジョンさんのアルバイト (John’s Part-time Job)</h2>
+      <section id="section1">
+        <h3>18.1: Transitivity Pairs</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>18.2: Paired Intransitive Verbs + ている</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>18.3: ~てしもう</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>18.4: ~と</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>18.5: ~ながら</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>18.6: ばよかったです</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
