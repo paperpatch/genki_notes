01ca5c9eaa982a3992ebaf27b6b178c0481db9d6
patch <patchen21@gmail.com>
2024-11-13 14:53:02 -0500

    lesson15

--- src/pages/Lesson15.js
+++ src/pages/Lesson15.js
@@ -1,8 +1,39 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson15() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 15</h2>
-      <p>Content 15 stuff</p>
+      <h2>Lesson 15: 長野旅行 (A Trip to Nagano)</h2>
+      <section id="section1">
+        <h3>15.1: Volitional Form</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>15.2: Volitional Form + と思っています</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>15.3: ~ておく</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>15.4: Using Sentences to Qualify Nouns</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
