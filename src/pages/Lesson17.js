005c767f23a4cfa831d7110eb7b1d47f63174c43
patch <patchen21@gmail.com>
2024-11-13 14:53:17 -0500

    lesson17

--- src/pages/Lesson17.js
+++ src/pages/Lesson17.js
@@ -1,8 +1,53 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson17() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 17</h2>
-      <p>Content 17 stuff</p>
+      <h2>Lesson 17: ぐちとうわさ話 (Grumble and Gossip)</h2>
+      <section id="section1">
+        <h3>17.1: ~そうです (I hear)</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>17.2: ~って</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>17.3: ~たら</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>17.4: ~なくてもいいです</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>17.5: ~みたいです</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>17.6: ~前に / ~てから</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
