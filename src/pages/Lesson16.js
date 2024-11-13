b795e1f6f66e2fc8453fe9c33098a1821cbf84b6
patch <patchen21@gmail.com>
2024-11-13 14:53:12 -0500

    lesson16

--- src/pages/Lesson16.js
+++ src/pages/Lesson16.js
@@ -1,8 +1,46 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson16() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 16</h2>
-      <p>Content 16 stuff</p>
+      <h2>Lesson 16: 忘れ物 (Lost and Found)</h2>
+      <section id="section1">
+        <h3>16.1: ~てあげる / てくれる / てもらう</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>16.2: ~ていただけませんか</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>16.3: といい</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>16.4: ~時</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>16.5: ~てすみませんでした</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
