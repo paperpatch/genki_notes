dc9a55e8853eeca3cc267765dacaad7ea7854d48
patch <patchen21@gmail.com>
2024-11-13 14:53:49 -0500

    lesson23

--- src/pages/Lesson23.js
+++ src/pages/Lesson23.js
@@ -1,8 +1,53 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson23() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 23</h2>
-      <p>Content 23 stuff</p>
+      <h2>Lesson 23: 別れ (Good-bye)</h2>
+      <section id="section1">
+        <h3>23.1: Causative-passive Sentences</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>23.2: ~ても</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>23.3: ~にとにする</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>23.4: ~にとにしている</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>23.5: ~まで</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>23.6: ~方</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
