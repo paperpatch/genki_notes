295738e94c0db1853bb41f6ed3aafadca117ff97
patch <patchen21@gmail.com>
2024-11-13 14:53:37 -0500

    lesson21

--- src/pages/Lesson21.js
+++ src/pages/Lesson21.js
@@ -1,8 +1,46 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson21() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 21</h2>
-      <p>Content 21 stuff</p>
+      <h2>Lesson 21: どろぼう (Burglar)</h2>
+      <section id="section1">
+        <h3>21.1: Passive Sentences</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>21.2: ~てある</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>21.3: ~間に</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>21.4: Adjective + する</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>21.5: ~てほしい</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
