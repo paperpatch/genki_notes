fa9cff05279924ea32f5f0dd27ca72e381b6df9a
patch <patchen21@gmail.com>
2024-11-13 14:52:29 -0500

    lesson9

--- src/pages/Lesson9.js
+++ src/pages/Lesson9.js
@@ -1,8 +1,59 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson9() {
+  useSmoothScrollToSection();
   return (
     <div>
-      <h2>Lesson 9</h2>
-      <p>Content 9 stuff</p>
+      <h2>Lesson 9: かぶき (Kabuki)</h2>
+      <section id="section1">
+        <h3>9.1: Past Tense Short Forms</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>9.2: Past Tense Short Forms in Informal Speech</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>9.3: Past Tense Short Forms in Quoted Speech: ~と思います</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>9.4: Past Tense Short Forms in Quoted Speech: ~と言っていました</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>9.5: Qualifying Nouns with Verbs and Adjectives</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>9.6: もう~ました and まだ~ていません</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section7">
+        <h3>9.7: Explanation から, Situation</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
