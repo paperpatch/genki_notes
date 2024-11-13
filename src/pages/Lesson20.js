00439d537533686fd9c07a2f8d39dc97cc40d717
patch <patchen21@gmail.com>
2024-11-13 14:53:31 -0500

    lesson20

--- src/pages/Lesson20.js
+++ src/pages/Lesson20.js
@@ -1,8 +1,53 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson20() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 20</h2>
-      <p>Content 20 stuff</p>
+      <h2>Lesson 20: メアリーさんの買い物 (Mary Goes Shopping)</h2>
+      <section id="section1">
+        <h3>20.1: Extra-modest Expressions</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>20.2: Humble Expressions</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>20.3: ないで</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>20.4: Questions within Larger Sentences</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>20.5: Name という Item</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>20.6: ~やすい / ~にくい</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
