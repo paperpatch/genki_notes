a7a8072632be012e65e5637bb2765bc9eb9e29e1
patch <patchen21@gmail.com>
2024-11-13 14:53:41 -0500

    lesson22

--- src/pages/Lesson22.js
+++ src/pages/Lesson22.js
@@ -1,8 +1,53 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson22() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 22</h2>
-      <p>Content 22 stuff</p>
+      <h2>Lesson 22: 日本の教育 (Education in Japan)</h2>
+      <section id="section1">
+        <h3>22.1: Causative Sentences</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>22.2: Causative + てあげる / てくれる / てもらう</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>22.3: Verb Stem + なさい</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>22.4: ~ば</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>22.5: ~のに</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>22.6: ~のように / ~のような</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
