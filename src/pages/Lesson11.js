f7b5f1aba76bd7e5d86da8e437b92622b18bdc67
patch <patchen21@gmail.com>
2024-11-13 14:52:44 -0500

    lesson11

--- src/pages/Lesson11.js
+++ src/pages/Lesson11.js
@@ -1,8 +1,39 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson11() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 11</h2>
-      <p>Content 11 stuff</p>
+      <h2>Lesson 11: 休みのあと (After the Vacation)</h2>
+      <section id="section1">
+        <h3>11.1: ~たい</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>11.2: ~たり~たりする</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>11.3: ~ことがある</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>11.4: Noun A や Noun B</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
