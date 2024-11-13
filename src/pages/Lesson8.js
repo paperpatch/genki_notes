dd7d9e08593b26f311a70ac25ca8a49594ee4f6d
patch <patchen21@gmail.com>
2024-11-13 14:52:21 -0500

    lesson8

--- src/pages/Lesson8.js
+++ src/pages/Lesson8.js
@@ -1,8 +1,67 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson8() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 8</h2>
-      <p>Content 8 stuff</p>
+      <h2>Lesson 8: バーベキュー (Barbecue)</h2>
+      <section id="section1">
+        <h3>8.1: Short Forms</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>8.2: Short Forms in Informal Speech</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>8.3: Short Forms in Quoted Speech: ~と思います</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>8.4: Short Forms in Quoted Speech: ~と言っていました</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>8.5: ~ないでください</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>8.6: のが好きです/上手です</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section7">
+        <h3>8.7: The Subject Particle が</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section8">
+        <h3>8.7: 何か and 何も</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
