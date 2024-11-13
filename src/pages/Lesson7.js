50b5cc219b92b9b277c1ce5d9c0e0745fdc2f96d
patch <patchen21@gmail.com>
2024-11-13 14:16:23 -0500

    lesson7

--- src/pages/Lesson7.js
+++ src/pages/Lesson7.js
@@ -1,8 +1,53 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson7() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 7</h2>
-      <p>Content 7 stuff</p>
+      <h2>Lesson 7: 家族の写真 (Family Picture)</h2>
+      <section id="section1">
+        <h3>7.1: ~ている (Action in Progress)</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>7.2: ~ている (Result of a Change)</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>7.3: メリーさんは髪が長いです</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>7.4: Adjective/Noun Te-forms for Joining Sentences</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>7.5: Verb Stem + に行く</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>7.6: Counting People</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
