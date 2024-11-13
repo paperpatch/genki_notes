9a532d339aed7dea6c79282dbfa072eba932586c
patch <patchen21@gmail.com>
2024-11-13 14:52:48 -0500

    lesson12

--- src/pages/Lesson12.js
+++ src/pages/Lesson12.js
@@ -1,8 +1,53 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson12() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 12</h2>
-      <p>Content 12 stuff</p>
+      <h2>Lesson 12: 病気 (Feeling ill)</h2>
+      <section id="section1">
+        <h3>12.1: ~んです</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>12.2: ~すぎる</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>12.3: ~ほうがいいです</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>12.4: ~ので</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>12.5: ~なければいけません / ~なきゃいけません</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>12.6: でしょうか</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
