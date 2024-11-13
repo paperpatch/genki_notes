490d93c049d8fb9c4be0367ea9b1b0ff77191591
patch <patchen21@gmail.com>
2024-11-13 14:52:57 -0500

    lesson14

--- src/pages/Lesson14.js
+++ src/pages/Lesson14.js
@@ -1,8 +1,46 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson14() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 14</h2>
-      <p>Content 14 stuff</p>
+      <h2>Lesson 14: バレンタインデー (Valentime’s Day)</h2>
+      <section id="section1">
+        <h3>14.1: ほしい</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>14.2: ~かもしれません</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>14.3: あげる / くれる / もらう</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>14.4: たらどうですか</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>14.5: Number + も / Number + しか + Negative</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
