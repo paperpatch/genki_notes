133f9a3f3e67dd7cae6fb147597588cc774a896f
patch <patchen21@gmail.com>
2024-11-13 14:52:35 -0500

    lesson10

--- src/pages/Lesson10.js
+++ src/pages/Lesson10.js
@@ -1,8 +1,60 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson10() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 10</h2>
-      <p>Content 10 stuff</p>
+      <h2>Lesson 10: 冬休みの予定 (Winter Vacations Plans)</h2>
+      <section id="section1">
+        <h3>10.1: Comparison between Two Items</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>10.2: Comparison among Three or More Items</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>10.3: Adjective/Noun + の</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>10.4: ~つもりだ</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>10.5: Adjective + なる</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>10.6: どこかに / どこにも</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section7">
+        <h3>10.7: で</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
