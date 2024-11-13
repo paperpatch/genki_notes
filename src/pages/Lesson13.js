26f52966c2fb4462610f4b5b8ddae062d240ae58
patch <patchen21@gmail.com>
2024-11-13 14:52:53 -0500

    lesson13

--- src/pages/Lesson13.js
+++ src/pages/Lesson13.js
@@ -1,8 +1,53 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson13() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 13</h2>
-      <p>Content 13 stuff</p>
+      <h2>Lesson 13: アルバイト探し (Looking for a Part-time Job)</h2>
+      <section id="section1">
+        <h3>13.1: Potential Verbs</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>13.2: ~し</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>13.3: ~そうです (It looks like...)</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>13.4: ~てみる</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>13.5: なら</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section6">
+        <h3>13.6: 一週間に三回</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
