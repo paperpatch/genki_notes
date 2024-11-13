cd09c3e3fd61adb1124b149635a453317692ca7d
patch <patchen21@gmail.com>
2024-11-13 14:53:26 -0500

    lesson19

--- src/pages/Lesson19.js
+++ src/pages/Lesson19.js
@@ -1,8 +1,46 @@
+import React from "react";
+import { useSmoothScrollToSection } from "../utils/Helpers";
+
 function Lesson19() {
+  useSmoothScrollToSection();
+
   return (
     <div>
-      <h2>Lesson 19</h2>
-      <p>Content 19 stuff</p>
+      <h2>Lesson 19: 出迎え (Meeting the Boss)</h2>
+      <section id="section1">
+        <h3>19.1: Honorific Verbs</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section2">
+        <h3>19.2: Giving Respectful Advice</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section3">
+        <h3>19.3: ~てくれてありがとう</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section4">
+        <h3>19.4: てよかったです</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
+
+      <section id="section5">
+        <h3>19.5: ~はずです</h3>
+        <div className="indent">
+          <p></p>
+        </div>
+      </section>
     </div>
   );
 }
