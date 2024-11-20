29e224965f2d036b31292bd8c6e421be1c902447
patch <patchen21@gmail.com>
2024-11-20 14:20:16 -0500

    lesson9.1

--- src/pages/Lesson9.js
+++ src/pages/Lesson9.js
@@ -9,7 +9,132 @@ function Lesson9() {
       <section id="section1">
         <h3>9.1: Past Tense Short Forms</h3>
         <div className="indent">
-          <p></p>
+          <p>
+            For past tense paradign of short forms, there are three rules to
+            understand:
+          </p>
+          <ol>
+            <li>
+              Verbs: Replace the <i>te</i>-form endings て and で with た and
+              だ.
+            </li>
+            <li>
+              い-adjectives: Replace the last い with かった. You may also think
+              of this in terms of "dropping です from the long form".
+            </li>
+            <li>
+              な-adjectives and nouns: Replace the long form でした ending with
+              だった.
+              <br />
+              Negation: ない conjugates as an い-adjective. Therefore, for all
+              categories, you can replace the last ない with なかった.
+            </li>
+          </ol>
+          <table className="table-list table-mini table-border-orange">
+            <thead>
+              <tr>
+                <th colSpan="2" className="table-header text-orange">
+                  Past, affirmative
+                </th>
+                <th className="table-header text-black">compare with:</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td rowSpan="2" style={{ verticalAlign: "top" }}>
+                  • verbs:
+                </td>
+                <td>
+                  食べ<span className="underline-orange">た</span>
+                </td>
+                <td>
+                  食べ<span className="underline-orange">て</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  読ん<span className="underline-orange">だ</span>
+                </td>
+                <td>
+                  読ん<span className="underline-orange">で</span>
+                </td>
+              </tr>
+              <tr>
+                <td>• い-adjectives:</td>
+                <td>
+                  かわい<span className="underline-orange">かった</span>
+                </td>
+                <td>
+                  かわいい / かわい
+                  <span className="underline-orange">かったです</span>
+                </td>
+              </tr>
+              <tr>
+                <td>• な-adjectives:</td>
+                <td>
+                  静か<span className="underline-orange">だった</span>
+                </td>
+                <td>
+                  静か<span className="underline-orange">でした</span>
+                </td>
+              </tr>
+              <tr>
+                <td>• noun + です:</td>
+                <td>
+                  学生<span className="underline-orange">だった</span>
+                </td>
+                <td>
+                  学生<span className="underline-orange">でした</span>
+                </td>
+              </tr>
+            </tbody>
+            <thead>
+              <tr>
+                <th colSpan="3" className="table-header text-orange">
+                  Past, negative
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>• verbs:</td>
+                <td>
+                  読んな<span className="underline-orange">かった</span>
+                </td>
+                <td>
+                  読んな<span className="underline-orange">い</span>
+                </td>
+              </tr>
+              <tr>
+                <td>• い-adjectives:</td>
+                <td>
+                  かわいくな<span className="underline-orange">かった</span>
+                </td>
+                <td>
+                  かわいくな
+                  <span className="underline-orange">い</span>
+                </td>
+              </tr>
+              <tr>
+                <td>• な-adjectives:</td>
+                <td>
+                  静かじゃな<span className="underline-orange">かった</span>
+                </td>
+                <td>
+                  静かじゃな<span className="underline-orange">い</span>
+                </td>
+              </tr>
+              <tr>
+                <td>• noun + です:</td>
+                <td>
+                  学生じゃな<span className="underline-orange">かった</span>
+                </td>
+                <td>
+                  学生じゃな<span className="underline-orange">い</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </section>
 
