feea28b877593cc47134b900140d4a2cd015420a
patch <patchen21@gmail.com>
2024-11-21 13:46:20 -0500

    lesson10.1

--- src/pages/Lesson10.js
+++ src/pages/Lesson10.js
@@ -10,7 +10,48 @@ function Lesson10() {
       <section id="section1">
         <h3>10.1: Comparison between Two Items</h3>
         <div className="indent">
-          <p></p>
+          <p>
+            In Japanese, adjectives do not change form in comparative sentences,
+            meaning there is no alteration as in "great/greater".
+          </p>
+          <table className="table-list table-mini table-border-orange">
+            <tbody>
+              <tr>
+                <td>
+                  A <span className="underline-orange">のほうが</span>
+                </td>
+                <td>
+                  B <span>より</span> (property)。
+                </td>
+                <td>=</td>
+                <td>
+                  <i>
+                    A <span className="underline-orange">is more</span>{" "}
+                    (property) <span className="underline-orange">than</span> B
+                  </i>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <p className="tab">
+            中国<span className="underline-orange">のほうが</span>日本
+            <span className="underline-orange">より</span>大きいです。
+            <br />
+            <i>China is larger than Japan.</i>
+          </p>
+          <p>If you want to ask a question comparing two items:</p>
+          <table className="table-list table-mini table-border-orange">
+            <tbody>
+              <tr>
+                <td>A と B と どちらのほう / どっちのほうが (property)。</td>
+              </tr>
+              <tr>
+                <td style={{ textAlign: "right" }}>
+                  <i>= Between A and B, which is more (property)?</i>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </section>
 
