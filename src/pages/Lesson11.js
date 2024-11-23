10d4b53c024afd041c91485df287eef3914ad2bc
patch <patchen21@gmail.com>
2024-11-22 22:36:33 -0500

    lesson11.1

--- src/pages/Lesson11.js
+++ src/pages/Lesson11.js
@@ -10,7 +10,196 @@ function Lesson11() {
       <section id="section1">
         <h3>11.1: ~たい</h3>
         <div className="indent">
-          <p></p>
+          <p>
+            Use a verb stem (before ます) + たいです to describe hope or
+            aspiration.
+          </p>
+          <p className="tab">
+            <span className="kanji">
+              今度<span className="furigana">こんど</span>
+            </span>
+            の
+            <span className="kanji">
+              週末<span className="furigana">しゅうまつ</span>
+            </span>
+            は、
+            <span className="kanji">
+              映画<span className="furigana">いえが</span>
+            </span>
+            を見<span className="underline-orange">たい</span>です。
+            <br />
+            <i>I want to see a film this weekend.</i>
+          </p>
+          <p className="tab">
+            いつか
+            <span className="kanji">
+              中国<span className="furigana">ちゅうごく</span>
+            </span>
+            に
+            <span className="kanji">
+              行<span className="furigana">い</span>
+            </span>
+            き<span className="underline-orange">たい</span>です。
+            <br />
+            <i>I want to go to China someday.</i>
+          </p>
+          <table className="table-list table-mini table-border-orange">
+            <tbody>
+              <tr>
+                <td>verb stem + たいです</td>
+                <td>
+                  <i>I want to do ...</i>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <p>
+            A verb that usually takes the particle を can either have the
+            particle を or が when it is followed by たい. Particles other than
+            を remain the same, like に.
+          </p>
+          <p>
+            たい conjugates as an い-adjective. Examples of negative and past
+            tense:
+          </p>
+          <p className="tab">
+            あの
+            <span className="kanji">
+              人<span className="furigana">ひと</span>
+            </span>
+            には
+            <span className="kanji">
+              会<span className="furigana">あ</span>
+            </span>
+            い<span className="underline-orange">たくないです</span>。
+            <br />
+            <i>I don't want to see that person.</i>
+          </p>
+          <p className="tab">
+            お
+            <span className="kanji">
+              弁当<span className="furigana">べんとう</span>
+            </span>
+            か
+            <span className="kanji">
+              買<span className="furigana">か</span>
+            </span>
+            い<span className="underline-orange">たかった</span>から、コンビニに
+            <span className="kanji">
+              行<span className="furigana">い</span>
+            </span>
+            きました。
+            <br />
+            <i>
+              I went to a convenience store, because I wanted to buy a boxed
+              lunch.
+            </i>
+          </p>
+          <p>
+            If you wanted something for some time, you can use たい
+            <span className="kanji">
+              思<span className="furigana">とも</span>
+            </span>
+            っています instead of たいです.
+          </p>
+          <p className="tab">
+            <span className="kanji">
+              留学<span className="furigana">りゅうがく</span>
+            </span>
+            し
+            <span className="underline-orange">
+              たいと
+              <span className="kanji">
+                思<span className="furigana">とも</span>
+              </span>
+              っています
+            </span>
+            。
+            <br />
+            <i>I've wanted to study abroad.</i>
+          </p>
+          <p>
+            You do not use たいです to describe wishes held by others.
+            Somebody's else' wants or wishes are usually in quotations,
+            observations, or guesses. If you want to say that somebody other
+            than you wants to do something, use と
+            <span className="kanji">
+              言<span className="furigana">い</span>
+            </span>
+            っていました with たい.
+          </p>
+          <p className="tab">
+            メアリーさんはチベットに
+            <span className="kanji">
+              行<span className="furigana">い</span>
+            </span>
+            き
+            <span className="underline-orange">
+              たい
+              <span className="kanji">
+                言<span className="furigana">い</span>
+              </span>
+              っていました
+            </span>
+            。
+            <br />
+            <i>Mary said she wanted to go to Tibet.</i>
+          </p>
+          <p>
+            To describe your observation that somebody wants to do something,
+            you use a special verb たがっている instead of たい. If a verb takes
+            the particle を, the derived verb がっている will retain を, unlike
+            たい.
+          </p>
+          <p className="tab">
+            メアリーさんは
+            <span className="kanji">
+              着物<span className="furigana">きもの</span>
+            </span>
+            <span className="underline-orange">を</span>
+            <span className="kanji">
+              着<span className="furigana">き</span>
+            </span>
+            <span className="underline-orange">たがっています</span>
+            。
+            <br />
+            <i>(It seems) Mary wants to wear a kimono.</i>
+          </p>
+          <p>
+            The verb たがっている, which comes from the dictionary form たがる,
+            indicates "I think that she wants to, because of the way she is
+            behaving."
+          </p>
+          <table className="table-list table-mini table-border-orange">
+            <tbody>
+              <tr>
+                <td>
+                  <i>I want to ... / Do you want to ... ?</i>
+                </td>
+                <td>
+                  <i>They want to ...</i>
+                </td>
+              </tr>
+              <tr>
+                <td>• verb stem + たいです</td>
+                <td>
+                  <i>• verb stem + たがっています</i>
+                </td>
+              </tr>
+              <tr>
+                <td>• たい conjugates as an い-adjective</td>
+                <td>
+                  <i>• たがる conjugates as an u-verb</i>
+                </td>
+              </tr>
+              <tr>
+                <td>• が or を for direct object</td>
+                <td>
+                  <i>• を only</i>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </section>
 
