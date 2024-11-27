a804a72ab69eeace5765cc7124a547013a4f2b17
patch <patchen21@gmail.com>
2024-11-26 23:00:02 -0500

    lesson14.2

--- src/pages/Lesson14.js
+++ src/pages/Lesson14.js
@@ -172,7 +172,7 @@ function Lesson14() {
               べたい。
             </p>
             <p>
-              ( not{" "}
+              ( <i>not</i>{" "}
               <span className="kanji">
                 昼<span className="furigana">ひる</span>
               </span>
@@ -194,7 +194,7 @@ function Lesson14() {
               に行きたい
             </p>
             <p>
-              ( not{" "}
+              ( <i>not</i>{" "}
               <span className="kanji">
                 旅<span className="furigana">りょ</span>
               </span>
@@ -210,7 +210,169 @@ function Lesson14() {
       <section id="section2">
         <h3>14.2: ~かもしれません</h3>
         <div className="indent">
-          <p></p>
+          <p>
+            かもしれません means that something is a "possibility". When you
+            think that something <i>may be</i> the case. When you say
+            かもしれません, you are not sure about the state of affairs than
+            when you say でしょう. In casual speech, you could say かもしれない
+            or かも.
+          </p>
+          <table className="table-list table-mini table-border-green">
+            <tbody>
+              <tr>
+                <td>short form + かもしれません</td>
+                <td>
+                  <i>Maybe ...</i>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <p>
+            You can add かもしれません to the short forms of predicates, in the
+            affirmative and in the negative, in the present as well as the past
+            tense.
+          </p>
+          <p className="tab">
+            あしたは
+            <span className="kanji">
+              雨<span className="furigana">あめ</span>
+            </span>
+            が
+            <span className="kanji">
+              降<span className="furigana">ふ</span>
+            </span>
+            るかもしれません。
+            <br />
+            <i>It may rain tomorrow.</i>
+          </p>
+          <p className="tab">
+            <span className="kanji">
+              田<span className="furigana">た</span>
+            </span>
+            <span className="kanji">
+              中<span className="furigana">なか</span>
+            </span>
+            さんより、
+            <span className="kanji">
+              鈴<span className="furigana">すず</span>
+            </span>
+            <span className="kanji">
+              木<span className="furigana">き</span>
+            </span>
+            さんのほうが
+            <span className="kanji">
+              背<span className="furigana">せ</span>
+            </span>
+            が
+            <span className="kanji">
+              高<span className="furigana">たか</span>
+            </span>
+            いかもしれない。
+            <br />
+            <i>Suzuki is perhaps taller than Tanaka.</i>
+          </p>
+          <p className="tab">
+            <span className="kanji">
+              明日<span className="furigana">あした</span>
+            </span>
+            は
+            <span className="kanji">
+              天<span className="furigana">てん</span>
+            </span>
+            <span className="kanji">
+              気<span className="furigana">き</span>
+            </span>
+            がよくないかも。
+            <br />
+            <i>The weather may not be good tomorrow.</i>
+          </p>
+          <p className="tab">
+            ロバートさんは、
+            <span className="kanji">
+              子<span className="furigana">こ</span>
+            </span>
+            <span className="kanji">
+              供<span className="furigana">ども</span>
+            </span>
+            の
+            <span className="kanji">
+              時<span className="furigana">とき</span>
+            </span>
+            、いじわるだったかもしれません。
+            <br />
+            <i>Robert may have been a bully when he was a kid.</i>
+          </p>
+          <p>
+            When the last part of a sentence is a noun or a な-adjective in the
+            present tense, put かもしれません directly after that noun or
+            な-adjective. だ is dropped in the sentences.
+          </p>
+          <div className="tab table-triple-column">
+            <p>
+              ウデイさんはインド
+              <span className="kanji">
+                人<span className="furigana">じん</span>
+              </span>
+              <span className="underline-green">だ</span>。
+              <br />
+              <i>Uday is an Indian.</i>
+            </p>
+            <p>→</p>
+            <p>
+              ウデイさんはインド
+              <span className="kanji">
+                人<span className="furigana">じん</span>
+              </span>
+              <span className="underline-green">かもしれません</span>。
+              <br />
+              <i>Uday may be an Indian.</i>
+            </p>
+          </div>
+          <div className="tab table-triple-column">
+            <p>
+              <span className="kanji">
+                山<span className="furigana">やま</span>
+              </span>
+              <span className="kanji">
+                下<span className="furigana">した</span>
+              </span>
+              <span className="kanji">
+                先<span className="furigana">せん</span>
+              </span>
+              <span className="kanji">
+                生<span className="furigana">せい</span>
+              </span>
+              は
+              <span className="kanji">
+                犬<span className="furigana">いぬ</span>
+              </span>
+              がきらい<span className="underline-green">だ</span>。
+              <br />
+              <i>Professor Yamashita doesn't like dogs.</i>
+            </p>
+            <p>→</p>
+            <p>
+              <span className="kanji">
+                山<span className="furigana">やま</span>
+              </span>
+              <span className="kanji">
+                下<span className="furigana">した</span>
+              </span>
+              <span className="kanji">
+                先<span className="furigana">せん</span>
+              </span>
+              <span className="kanji">
+                生<span className="furigana">せい</span>
+              </span>
+              は
+              <span className="kanji">
+                犬<span className="furigana">いぬ</span>
+              </span>
+              がきらい<span className="underline-green">かもしれません</span>。
+              <br />
+              <i>Professor Yamashita may not like dogs.</i>
+            </p>
+          </div>
         </div>
       </section>
 
