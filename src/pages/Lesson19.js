363d7652b82fd377eb6c2f67e92983dc0d3007f4
patch <patchen21@gmail.com>
2024-12-07 15:17:07 -0500

    lesson19.1

--- src/pages/Lesson19.js
+++ src/pages/Lesson19.js
@@ -10,7 +10,388 @@ function Lesson19() {
       <section id="section1">
         <h3>19.1: Honorific Verbs</h3>
         <div className="indent">
-          <p></p>
+          <p>
+            We use special verbs to describe the actions of people whom you
+            respect. These special verbs are called honorific verbs, because
+            they bestow honor on, or exalt, the person performing the activity.
+          </p>
+          <table className="table-list table-border-green">
+            <tbody>
+              <tr>
+                <td></td>
+                <td>
+                  <span className="text-green">Honorific verbs</span>
+                </td>
+                <td>
+                  <span className="text-green">Irregular conjugations</span>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="9" className="horizontal-line-wrapper">
+                  <div className="horizontal-line-green"></div>
+                </td>
+              </tr>
+              <tr>
+                <td className="text-blue">いる</td>
+                <td rowSpan={3} className="text-blue">
+                  いらっしゃる
+                </td>
+                <td rowSpan={3} className="text-blue">
+                  いらっしゃ<span className="underline-green">い</span>ます
+                </td>
+              </tr>
+              <tr>
+                <td className="text-blue">
+                  <span className="kanji">
+                    行<span className="furigana">い</span>
+                  </span>
+                  く
+                </td>
+              </tr>
+              <tr>
+                <td className="text-blue">
+                  <span className="kanji">
+                    来<span className="furigana">く</span>
+                  </span>
+                  る
+                </td>
+              </tr>
+              <tr>
+                <td className="text-purple">
+                  <span className="kanji">
+                    食<span className="furigana">た</span>
+                  </span>
+                  べる
+                </td>
+                <td rowSpan={2} className="text-purple">
+                  <span className="kanji">
+                    召<span className="furigana">め</span>
+                  </span>
+                  し
+                  <span className="kanji">
+                    上<span className="furigana">あ</span>
+                  </span>
+                  がる
+                </td>
+              </tr>
+              <tr>
+                <td className="text-purple">
+                  <span className="kanji">
+                    飲<span className="furigana">の</span>
+                  </span>
+                  む
+                </td>
+              </tr>
+              <tr>
+                <td>くれる</td>
+                <td>くださる</td>
+                <td>
+                  くださ<span className="underline-green">い</span>ます
+                </td>
+              </tr>
+              <tr>
+                <td>する</td>
+                <td>なさる</td>
+                <td>
+                  なさ<span className="underline-green">い</span>ます
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span className="kanji">
+                    寝<span className="furigana">ね</span>
+                  </span>
+                  る
+                </td>
+                <td>
+                  お
+                  <span className="kanji">
+                    休<span className="furigana">やす</span>
+                  </span>
+                  みになる
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span className="kanji">
+                    見<span className="furigana">み</span>
+                  </span>
+                  る
+                </td>
+                <td>
+                  ご
+                  <span className="kanji">
+                    覧<span className="furigana">らん</span>
+                  </span>
+                  になる
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span className="kanji">
+                    言<span className="furigana">い</span>
+                  </span>
+                  う
+                </td>
+                <td>おっしゃる</td>
+                <td>
+                  おっしゃ<span className="underline-green">い</span>ます
+                </td>
+              </tr>
+              <tr>
+                <td>～ている</td>
+                <td>～ていらっしゃる</td>
+                <td>
+                  ～ていらっしゃ<span className="underline-green">い</span>ます
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <p>
+            All the honorific verbs listed above are <i>u</i>-verbs, but some of
+            them have irregular conjugations. The long forms of いらっしゃる,
+            おっしゃる, なさる, くださる, and ～ていらっしゃる end with います,
+            instead of the expected ります.
+          </p>
+          <p>
+            When we use an honorific verb instead of a normal verb, we will have
+            sentences which mean that somebody graciously does something. Thus,
+            we never use these verbs to describe our own actions. Instead, we
+            use them when we talk about what is done by (1) somebody higher up
+            in the social hierarchy, or (2) somebody whom you do not know very
+            well, especially when addressing them directly.
+          </p>
+          <p className="tab">
+            <span className="kanji">
+              先生<span className="furigana">せんせい</span>
+            </span>
+            は
+            <span className="kanji">
+              今日<span className="furigana">きょう</span>
+            </span>
+            <span className="kanji">
+              学校<span className="furigana">がっこう</span>
+            </span>
+            に<span className="underline-green">いらっしゃいません</span>
+            。　cf. 行きません・きません・いません
+            <br />
+            <i>
+              The professor will (graciously) not go to/come to/be at the
+              school.
+            </i>
+            　(three-way ambiguous)
+          </p>
+          <p className="tab">
+            <span className="kanji">
+              何<span className="furigana">なに</span>
+            </span>
+            を
+            <span className="underline-green">
+              <span className="kanji">
+                召<span className="furigana">め</span>
+              </span>
+              し
+              <span className="kanji">
+                上<span className="furigana">あ</span>
+              </span>
+              がります
+            </span>
+            か。　cf. 食べますか・飲みますか
+            <br />
+            <i>What will you (graciously) eat/drink?</i>
+          </p>
+          <p className="tab">
+            <span className="kanji">
+              田<span className="furigana">た</span>
+            </span>
+            <span className="kanji">
+              中<span className="furigana">なか</span>
+            </span>
+            さんのお
+            <span className="kanji">
+              母<span className="furigana">かあ</span>
+            </span>
+            さんがこの
+            <span className="kanji">
+              本<span className="furigana">ほん</span>
+            </span>
+            を<span className="underline-green">くださいました</span>。　cf.
+            くれました
+            <br />
+            <i>Ms. Tanaka's mother (graciously) gave me this book.</i>
+          </p>
+          <p className="tab">
+            <span className="kanji">
+              心<span className="furigana">しん</span>
+            </span>
+            <span className="kanji">
+              配<span className="furigana">ぱい</span>
+            </span>
+            <span className="underline-green">なさらないで</span>ください。
+            　cf.{" "}
+            <span className="kanji">
+              心<span className="furigana">しん</span>
+            </span>
+            <span className="kanji">
+              配<span className="furigana">ぱい</span>
+            </span>
+            しないでください
+            <br />
+            <i>Please don't (graciously) worry.</i>
+          </p>
+          <p className="tab">
+            <span className="kanji">
+              先生<span className="furigana">せんせい</span>
+            </span>
+            は
+            <span className="kanji">
+              十時<span className="furigana">じゅうじ</span>
+            </span>
+            ごろ
+            <span className="underline-green">
+              お
+              <span className="kanji">
+                休<span className="furigana">やす</span>
+              </span>
+              みになる
+            </span>
+            そうです。 　cf.{" "}
+            <span className="kanji">
+              寝<span className="furigana">ね</span>
+            </span>
+            るそうです
+            <br />
+            <i>Please don't (graciously) worry.</i>
+          </p>
+          <p className="tab">
+            <span className="kanji">
+              社長<span className="furigana">しゃちょう</span>
+            </span>
+            はニュースを
+            <span className="underline-green">
+              ご
+              <span className="kanji">
+                覧<span className="furigana">らん</span>
+              </span>
+              になって
+            </span>
+            います。　cf.{" "}
+            <span className="kanji">
+              見<span className="furigana">み</span>
+            </span>
+            ています
+            <br />
+            <i>The president is (graciously) watching the news.</i>
+          </p>
+          <p>
+            For the activities for which we lack special honorific verbs, we add
+            the respect factor as follows:
+          </p>
+          <p>
+            (1) Using ていらっしゃいます instead of ています, if the sentence
+            has the helping verb ている.
+          </p>
+          <p className="tab">
+            <span className="kanji">
+              先生<span className="furigana">せんせい</span>
+            </span>
+            は
+            <span className="kanji">
+              電<span className="furigana">でん</span>
+            </span>
+            <span className="kanji">
+              話<span className="furigana">わ</span>
+            </span>
+            で
+            <span className="kanji">
+              話<span className="furigana">はな</span>
+            </span>
+            し<span className="underline-green">ていらっしゃいます</span>
+            。　（＜　
+            <span className="kanji">
+              話<span className="furigana">はな</span>
+            </span>
+            し<span className="underline-green">ています</span>）
+            <br />
+            <i>The president is (graciously) talking on the phone.</i>
+          </p>
+          <p className="tab">
+            <span className="kanji">
+              部<span className="furigana">ぶ</span>
+            </span>
+            <span className="kanji">
+              長<span className="furigana">ちょう</span>
+            </span>
+            は
+            <span className="kanji">
+              使<span className="furigana">つか</span>
+            </span>
+            れ<span className="underline-green">ていらっしゃる</span>
+            みたいです。　（＜　
+            <span className="kanji">
+              使<span className="furigana">つか</span>
+            </span>
+            れ<span className="underline-green">ている</span>みたいです）
+            <br />
+            <i>It appears that the department manager is (graciously) tired.</i>
+          </p>
+          <p>(2) Flanking a verb stem with お and になる.</p>
+          <table className="table-list table-mini table-border-green">
+            <tbody>
+              <tr>
+                <td>お　＋　verb stem　＋　になる</td>
+              </tr>
+            </tbody>
+          </table>
+          <p className="tab">
+            <span className="kanji">
+              先生<span className="furigana">せんせい</span>
+            </span>
+            はもうお
+            <span className="kanji">
+              帰<span className="furigana">かえ</span>
+            </span>
+            りになりました。　（＜　
+            <span className="kanji">
+              帰<span className="furigana">かえ</span>
+            </span>
+            りました）
+            <br />
+            <i>The professor has already (graciously) gone home.</i>
+          </p>
+          <p className="tab">
+            この
+            <span className="kanji">
+              雑<span className="furigana">ざっ</span>
+            </span>
+            <span className="kanji">
+              誌<span className="furigana">し</span>
+            </span>
+            をお
+            <span className="kanji">
+              読<span className="furigana">よ</span>
+            </span>
+            みになったことがありますか。　（＜　
+            <span className="kanji">
+              読<span className="furigana">よ</span>
+            </span>
+            んだことがありますか）
+            <br />
+            <i>Have you ever (graciously) read this magazine?</i>
+          </p>
+          <p className="tab">
+            どうぞお
+            <span className="kanji">
+              使<span className="furigana">つか</span>
+            </span>
+            いになってください。　（＜　
+            <span className="kanji">
+              使<span className="furigana">つか</span>
+            </span>
+            ってください）
+            <br />
+            <i>Please (graciously) use it.</i>
+          </p>
         </div>
       </section>
 
