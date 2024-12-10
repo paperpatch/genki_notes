23111a9a6468906aec0d247a9e88e9ddcc6f3e68
patch <patchen21@gmail.com>
2024-12-09 21:37:03 -0500

    lesson20.2

--- src/pages/Lesson20.js
+++ src/pages/Lesson20.js
@@ -347,7 +347,357 @@ function Lesson20() {
       <section id="section2">
         <h3>20.2: Humble Expressions</h3>
         <div className="indent">
-          <p></p>
+          <p>
+            When you do something out of respect for somebody, you can sometimes
+            describe your action using a verb in the humble pattern "お + verb
+            stem + する." Not all verbs use this pattern, so use the ones that
+            you are familiar with. You can speak of "humbly" meeting, leading
+            to, or borrowing from someone, for example.
+          </p>
+          <table className="table-list table-mini table-border-green">
+            <tbody>
+              <tr>
+                <td>お + verb stem + する</td>
+                <td>
+                  <i>I (humbly) do...</i>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <p className="tab">
+            私は
+            <span className="kanji">
+              昨日<span className="furigana">きのう</span>
+            </span>
+            <span className="underline-green">
+              <span className="kanji">
+                社長<span className="furigana">しゃちょう</span>
+              </span>
+              のご
+              <span className="kanji">
+                主人<span className="furigana">しゅじん</span>
+              </span>
+              に
+            </span>
+            お
+            <span className="kanji">
+              会<span className="furigana">あ</span>
+            </span>
+            いしました。
+            <br />
+            <i>I (humbly) met the president's husband yesterday.</i>
+          </p>
+          <p className="tab">
+            私は
+            <span className="underline-green">
+              <span className="kanji">
+                先生<span className="furigana">せんせい</span>
+              </span>
+              に
+            </span>
+            お
+            <span className="kanji">
+              本<span className="furigana">ほん</span>
+            </span>
+            をお
+            <span className="kanji">
+              貸<span className="furigana">か</span>
+            </span>
+            しするつもりです。
+            <br />
+            <i>I intend to (humbly) lend my professor a book.</i>
+          </p>
+          <p className="tab">
+            私は
+            <span className="underline-green">
+              <span className="kanji">
+                先生<span className="furigana">せんせい</span>
+              </span>
+              に
+            </span>
+            <span className="kanji">
+              辞書<span className="furigana">じしょ</span>
+            </span>
+            をお
+            <span className="kanji">
+              借<span className="furigana">か</span>
+            </span>
+            りしました。
+            <br />
+            <i>
+              I borrowed a dictionary from my professor (and feel very obliged).
+            </i>
+          </p>
+          <p>
+            Note that this "お + verb stem + する" pattern demotes the action (I
+            humble do something) while the "お + stem + になる" pattern promotes
+            the actor (Somebody graciously does something). They look similar,
+            but semantically they are polar opposites.
+          </p>
+          <table className="table-mini">
+            <tbody>
+              <tr>
+                <td className="no-wrap text-green">する</td>
+                <td style={{ paddingLeft: "1.5em" }}>
+                  する compound verbs do not follow this pattern. Instead they
+                  simply have the prefix ご or お, such as ご
+                  <span className="kanji">
+                    紹介<span className="furigana">しょうかい</span>
+                  </span>
+                  する, ご
+                  <span className="kanji">
+                    案内<span className="furigana">あんない</span>
+                  </span>
+                  する, ご
+                  <span className="kanji">
+                    説明<span className="furigana">せつめい</span>
+                  </span>
+                  する, and お
+                  <span className="kanji">
+                    電話<span className="furigana">でんわ</span>
+                  </span>
+                  する.
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <p className="tab">
+            <span className="kanji">
+              外国人<span className="furigana">がいこくじん</span>
+            </span>
+            に
+            <span className="kanji">
+              人気<span className="furigana">にんき</span>
+            </span>
+            がある
+            <span className="kanji">
+              店<span className="furigana">みせ</span>
+            </span>
+            をご しましょう。
+            <br />
+            <i>
+              Let us tell you about a shop that is popular among foreigners.
+            </i>
+          </p>
+          <p className="tab">
+            きのうお
+            <span className="kanji">
+              電話<span className="furigana">でんわ</span>
+            </span>
+            した田中です。
+            <br />
+            <i>This is Tanaka. I called yesterday.</i>
+          </p>
+          <table className="table-mini">
+            <tbody>
+              <tr>
+                <td className="text-green">もらう ／ あげる</td>
+                <td style={{ paddingLeft: "1.5em" }}>
+                  もらう and あげる have special replacement verbs:
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <table className="table-list table-mini">
+            <tbody>
+              <tr>
+                <td>もらう</td>
+                <td className="arrow-right">→</td>
+                <td>いただく</td>
+                <td>
+                  私は
+                  <span className="underline-green">
+                    <span className="kanji">
+                      先生<span className="furigana">せんせい</span>
+                    </span>
+                    に
+                  </span>
+                  この{" "}
+                  <span className="kanji">
+                    本<span className="furigana">ほん</span>
+                  </span>
+                  をいただきました。
+                  <br />
+                  <i>I (humbly) received this book from my professor.</i>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan={3}></td>
+                <td>
+                  私
+                  <span className="underline-green">
+                    <span className="kanji">
+                      先生<span className="furigana">せんせい</span>
+                    </span>
+                    に
+                  </span>
+                  <span className="kanji">
+                    漢字<span className="furigana">かんじ</span>
+                  </span>
+                  を
+                  <span className="kanji">
+                    教<span className="furigana">おし</span>
+                  </span>
+                  えていただきました。
+                  <br />
+                  <i>I (humbly) had my professor teach me kanji.</i>
+                </td>
+              </tr>
+              <tr>
+                <td>あげる</td>
+                <td className="arrow-right">→</td>
+                <td>さしあげる</td>
+                <td>
+                  私は
+                  <span className="underline-green">
+                    <span className="kanji">
+                      先生<span className="furigana">せんせい</span>
+                    </span>
+                    に
+                  </span>
+                  <span className="kanji">
+                    花<span className="furigana">はな</span>
+                  </span>
+                  をさしあげます。
+                  <br />
+                  <i>I will (humbly) give my professor flowers.</i>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <table className="table-mini">
+            <tbody>
+              <tr>
+                <td className="text-green">うかがう</td>
+                <td style={{ paddingLeft: "1.5em" }}>
+                  うかがう is a verb with which you can portray yourself as
+                  humble in the actions of visiting and asking questions:
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <p className="tab">
+            私は
+            <span className="underline-green">
+              <span className="kanji">
+                部長<span className="furigana">ぶちょう</span>
+              </span>
+              の
+            </span>
+            お
+            <span className="kanji">
+              宅<span className="furigana">たく</span>
+            </span>
+            にうかがいました。
+            <br />
+            <i>I (humbly) visited my department manager's house.</i>
+          </p>
+          <p className="tab">
+            私は
+            <span className="underline-green">
+              <span className="kanji">
+                先生<span className="furigana">せんせい</span>
+              </span>
+              に
+            </span>
+            テストについてうかがいました。
+            <br />
+            <i>I (humbly) asked my professor about the exam.</i>
+          </p>
+          <p>
+            The subject with "I" performs the action in deference to the person
+            underlined. The difference between this pattern and the extra-modest
+            expressions is that the extra-modest expressions show respect to the
+            listeners you are talking to, while the humble pattern shows respect
+            to someone that appears in the event you are describing. For
+            example:
+          </p>
+          <p className="tab">
+            （私はあなたを）
+            <span className="kanji">
+              駅<span className="furigana">えき</span>
+            </span>
+            までお
+            <span className="kanji">
+              送<span className="furigana">おく</span>
+            </span>
+            りします。
+            <br />
+            <i>I will (humbly) walk you to the station.</i>
+          </p>
+          <p>
+            Summary of the three types of "respect language", with the up and
+            down arrow indicating the person whose profile is raised or lowered,
+            respectively.
+          </p>
+          <ol>
+            <li>
+              <strong>Honorific expressions</strong> exalt the subject of the
+              sentence.
+              <br />
+              <span className="kanji">
+                先生<span className="furigana">せんせい</span>
+              </span>
+              が{" "}
+              <span className="text-green" style={{ fontSize: "1.4em" }}>
+                ↑
+              </span>{" "}
+              いらっしゃいました／お
+              <span className="kanji">
+                帰<span className="furigana">かえ</span>
+              </span>
+              りになりました。
+              <br />
+              <i>My professor has (graciously) arrived/left.</i>
+            </li>
+            <li>
+              <strong>Extra-modest expressions</strong> talk modestly of what
+              you do.
+              <br />
+              <span className="kanji">
+                私<span className="furigana">わたくし</span>
+              </span>
+              は{" "}
+              <span className="text-green" style={{ fontSize: "1.4em" }}>
+                ↓
+              </span>{" "}
+              メアリー・ハートと申します。(person listening to you{" "}
+              <span className="text-green" style={{ fontSize: "1.4em" }}>
+                ↑
+              </span>{" "}
+              )<br />
+              <i>My name is Mary Hart.</i>
+            </li>
+            <li>
+              <strong>Humble expressions</strong> demote the subject and raise
+              the profile of another person.
+              <br />
+              <span className="kanji">
+                私<span className="furigana">わたし</span>
+              </span>
+              は{" "}
+              <span className="text-green" style={{ fontSize: "1.4em" }}>
+                ↓
+              </span>{" "}
+              <span className="kanji">
+                先生<span className="furigana">せんせい</span>
+              </span>
+              に{" "}
+              <span className="text-green" style={{ fontSize: "1.4em" }}>
+                ↑
+              </span>{" "}
+              <span className="kanji">
+                本<span className="furigana">ほん</span>
+              </span>
+              をお
+              <span className="kanji">
+                返<span className="furigana">かえ</span>
+              </span>
+              ししました。
+              <br />
+              <i>I (humbly) returned the book to my professor.</i>
+            </li>
+          </ol>
         </div>
       </section>
 
