11448ab4079da9f95f276e3f7f63b757b1eb2efd
patch <patchen21@gmail.com>
2024-12-18 16:55:49 -0500

    lesson23.1

--- src/pages/Lesson23.js
+++ src/pages/Lesson23.js
@@ -10,7 +10,412 @@ function Lesson23() {
       <section id="section1">
         <h3>23.1: Causative-passive Sentences</h3>
         <div className="indent">
-          <p></p>
+          <p>
+            "Causative-passive" sentences are the passive version of causative
+            sentences. You can use causative-passive sentences when you want to
+            say that you were made to do, or haraased or talked into doing
+            something you did not want to.
+          </p>
+          <p className="tab">
+            （
+            <span className="kanji">
+              下<span className="furigana">へ</span>
+            </span>
+            <span className="kanji">
+              手<span className="furigana">た</span>
+            </span>
+            がから
+            <span className="kanji">
+              歌<span className="furigana">うた</span>
+            </span>
+            いたくなかったのに）
+            <span className="kanji">
+              歌<span className="furigana">うた</span>
+            </span>
+            を
+            <span className="kanji">
+              歌<span className="furigana">うた</span>
+            </span>
+            わされました。
+            <br />
+            <i>
+              (I didn't want to sing because I'm not a good singer, but) I was
+              forced to sing.
+            </i>
+          </p>
+          <p>You can make the causative-passive forms in the following:</p>
+          <table className="table-list table-mini table-border-green">
+            <thead>
+              <tr>
+                <th colSpan="6" className="table-header text-green">
+                  Causative-passive form
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td colSpan="6">
+                  • ru-verbs: Drop the final -<i>ru</i> and add -
+                  <i>sase-rare-ru</i>.
+                </td>
+              </tr>
+              <tr>
+                <td style={{ paddingLeft: "2em" }}>
+                  <span className="kanji">
+                    食<span className="furigana">た</span>
+                  </span>
+                  べる
+                </td>
+                <td className="arrow-right">→</td>
+                <td>
+                  <span className="kanji">
+                    食<span className="furigana">た</span>
+                  </span>
+                  べさせられる
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="6">
+                  • u-verbs that end with す: Drop -<i>u</i> and add -
+                  <i>ase-rare-ru</i>.
+                </td>
+              </tr>
+              <tr>
+                <td style={{ paddingLeft: "2em" }}>
+                  <span className="kanji">
+                    話<span className="furigana">はな</span>
+                  </span>
+                  す
+                </td>
+                <td className="arrow-right">→</td>
+                <td>
+                  <span className="kanji">
+                    話<span className="furigana">はな</span>
+                  </span>
+                  させられる
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="6">
+                  • all other u-verbs: Drop -<i>u</i> and add -<i>asare-ru</i>.
+                </td>
+              </tr>
+              <tr>
+                <td style={{ paddingLeft: "2em" }}>
+                  <span className="kanji">
+                    行<span className="furigana">い</span>
+                  </span>
+                  く
+                </td>
+                <td className="arrow-right">→</td>
+                <td>
+                  <span className="kanji">
+                    行<span className="furigana">い</span>
+                  </span>
+                  かされる
+                </td>
+                <td>
+                  <span className="kanji">
+                    泳<span className="furigana">およ</span>
+                  </span>
+                  ぐ
+                </td>
+                <td className="arrow-right">→</td>
+                <td>
+                  <span className="kanji">
+                    泳<span className="furigana">およ</span>
+                  </span>
+                  がされる
+                </td>
+              </tr>
+              <tr>
+                <td style={{ paddingLeft: "2em" }}>
+                  <span className="kanji">
+                    待<span className="furigana">ま</span>
+                  </span>
+                  つ
+                </td>
+                <td className="arrow-right">→</td>
+                <td>
+                  <span className="kanji">
+                    待<span className="furigana">ま</span>
+                  </span>
+                  たされる
+                </td>
+                <td>
+                  <span className="kanji">
+                    遊<span className="furigana">あそ</span>
+                  </span>
+                  ぶ
+                </td>
+                <td className="arrow-right">→</td>
+                <td>
+                  <span className="kanji">
+                    遊<span className="furigana">あそ</span>
+                  </span>
+                  ばされる
+                </td>
+              </tr>
+              <tr>
+                <td style={{ paddingLeft: "2em" }}>
+                  <span className="kanji">
+                    読<span className="furigana">よ</span>
+                  </span>
+                  む
+                </td>
+                <td className="arrow-right">→</td>
+                <td>
+                  <span className="kanji">
+                    読<span className="furigana">よ</span>
+                  </span>
+                  まされる
+                </td>
+                <td>
+                  <span className="kanji">
+                    買<span className="furigana">か</span>
+                  </span>
+                  う
+                </td>
+                <td className="arrow-right">→</td>
+                <td>
+                  <span className="kanji">
+                    買<span className="furigana">か</span>
+                  </span>
+                  <span className="underline-green">わ</span>される
+                </td>
+              </tr>
+              <tr>
+                <td style={{ paddingLeft: "2em" }}>
+                  <span className="kanji">
+                    取<span className="furigana">と</span>
+                  </span>
+                  る
+                </td>
+                <td className="arrow-right">→</td>
+                <td>
+                  <span className="kanji">
+                    取<span className="furigana">と</span>
+                  </span>
+                  らされる
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="6">• irregular verbs:</td>
+              </tr>
+              <tr>
+                <td style={{ paddingLeft: "2em" }}>する</td>
+                <td className="arrow-right">→</td>
+                <td>させられる</td>
+                <td>くる</td>
+                <td className="arrow-right">→</td>
+                <td>こさせられる</td>
+              </tr>
+            </tbody>
+          </table>
+          <p>
+            Notice tha tthe causative-passive in <i>ru</i>-verbs, <i>u</i>-verbs
+            ending with す, and irregular verbs is the combination of causative
+            and the passive forms: -<i>(s)ase-rare</i>. In <i>u</i>-verbs other
+            than those that end with す, the causative passive suffix -
+            <i>asare</i> is shorter than the sum of the causative (-<i>ase</i>)
+            and the passive (-<i>rare</i>) suffixes.
+          </p>
+          <p>Basic structure of a causative-passive sentence:</p>
+          <table className="table-list table-mini table-border-green">
+            <thead>
+              <tr>
+                <th className="table-header text-green">
+                  Causative-passive sentence
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td style={{ paddingBottom: "5px", paddingLeft: "3em" }}>
+                  <span className="kanji">
+                    私<span className="furigana">わたし</span>
+                  </span>
+                  は
+                </td>
+                <td style={{ paddingBottom: "5px" }}>
+                  <span className="kanji">
+                    彼女<span className="furigana">かのじょ</span>
+                  </span>
+                  に
+                </td>
+                <td style={{ paddingBottom: "5px" }}>
+                  <span className="kanji">
+                    車<span className="furigana">くるま</span>
+                  </span>
+                  を
+                  <span className="kanji">
+                    洗<span className="furigana">あら</span>
+                  </span>
+                  わされました。
+                </td>
+              </tr>
+              <tr>
+                <td style={{ paddingTop: 0, paddingLeft: "3em" }}>
+                  <span className="text-green">(puppet)</span> は
+                </td>
+                <td style={{ paddingTop: 0 }}>
+                  <span className="text-green">(puppet master)</span> に
+                </td>
+                <td style={{ paddingTop: 0 }}>
+                  <span className="text-green">(action)</span>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan={3} style={{ paddingLeft: "3em" }}>
+                  <i>I was tricked by my girlfriend into washing her car.</i>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan={3} style={{ paddingBottom: 0 }}>
+                  <ul style={{ padding: 0, margin: 0 }}>
+                    <li>
+                      • The <span className="text-green">puppet</span> is forced
+                      into performing an action. Marked with は or が.
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan={3} style={{ paddingBottom: 0 }}>
+                  <ul style={{ padding: 0, margin: 0 }}>
+                    <li>
+                      • The <span className="text-green">puppet master</span>{" "}
+                      wields power over, and manipulates, the puppet. Marked
+                      with に.
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan={3}>
+                  <ul style={{ padding: 0, margin: 0 }}>
+                    <li>
+                      • The <span className="text-green">action</span> forced
+                      upon the puppet is described with a causative-passive
+                      verb.
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <p>
+            If you compare a causative-passive sentence with a causative
+            sentence, notice that the actors are switched between the two:
+          </p>
+          <table className="table-list table-mini">
+            <tbody>
+              <tr>
+                <td>Causative-passive:</td>
+                <td>
+                  <span className="border-green">私</span>　
+                  <span className="underline-green">は</span>　
+                  <span className="border-green">
+                    <span className="kanji">
+                      友達<span className="furigana">ともだち</span>
+                    </span>
+                  </span>
+                  　<span className="underline-green">に</span>
+                </td>
+                <td>
+                  <span className="kanji">
+                    宿題<span className="furigana">しゅくだい</span>
+                  </span>
+                  を
+                  <span className="kanji">
+                    手伝<span className="furigana">てつだ</span>
+                  </span>
+                  わされました。
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colSpan={2} style={{ paddingTop: 0 }}>
+                  I was forced by my friend into helping with with his homework.
+                </td>
+              </tr>
+              <tr>
+                <td>Causative:</td>
+                <td>
+                  <span className="border-green">
+                    <span className="kanji">
+                      友達<span className="furigana">ともだち</span>
+                    </span>
+                  </span>
+                  　<span className="underline-green">は</span>　
+                  <span className="border-green">私</span>　
+                  <span className="underline-green">に</span>
+                </td>
+                <td>
+                  <span className="kanji">
+                    宿題<span className="furigana">しゅくだい</span>
+                  </span>
+                  を
+                  <span className="kanji">
+                    手伝<span className="furigana">てつだ</span>
+                  </span>
+                  わされました。
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colSpan={2} style={{ paddingTop: 0 }}>
+                  My friend made me help him with his homework.
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <p>
+            Compare a causative-passive sentence with a plain (noncausative,
+            nonpassive) sentence. These two sentences have the same subject. You
+            add the "puppet master" role to a plain sentence and make the verb
+            longer, and you get a causative-passive sentence.
+          </p>
+          <table className="table-list table-mini">
+            <tbody>
+              <tr>
+                <td>Causative-passive:</td>
+                <td>
+                  まなは　お
+                  <span className="kanji">
+                    母<span className="furigana">かあ</span>
+                  </span>
+                  さんに　
+                  <span className="kanji">
+                    勉強<span className="furigana">べんきょう</span>
+                  </span>
+                  させられました。
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td style={{ paddingTop: 0 }}>
+                  <i>Mana was ordered by her mother to study.</i>
+                </td>
+              </tr>
+              <tr>
+                <td>Plain:</td>
+                <td>
+                  まなは　―――――　
+                  <span className="kanji">
+                    勉強<span className="furigana">べんきょう</span>
+                  </span>
+                  しました。
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td style={{ paddingTop: 0 }}>
+                  <i>Mana studied.</i>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </section>
 
