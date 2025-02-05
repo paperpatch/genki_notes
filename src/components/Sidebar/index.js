4e1dc576c4d41fe0b4152ef5a92df63172f8deba
patch <patchen21@gmail.com>
2025-02-05 16:01:18 -0500

    sidebar toc highlight

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -88,40 +88,64 @@ function Table() {
             </button>
             {genkiOneOpen && (
               <ul className="nested">
-                <li>
+                <li
+                  className={location.pathname === "/lesson1" ? "active" : ""}
+                >
                   <Link to="/lesson1">Lesson 1: New Friends</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson2" ? "active" : ""}
+                >
                   <Link to="/lesson2">Lesson 2: Shopping</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson3" ? "active" : ""}
+                >
                   <Link to="/lesson3">Lesson 3: Making a Date</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson4" ? "active" : ""}
+                >
                   <Link to="/lesson4">Lesson 4: The First Date</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson5" ? "active" : ""}
+                >
                   <Link to="/lesson5">Lesson 5: A Trip to Okinawa</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson6" ? "active" : ""}
+                >
                   <Link to="/lesson6">Lesson 6: A Day in Robert's Life</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson7" ? "active" : ""}
+                >
                   <Link to="/lesson7">Lesson 7: Family Picture</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson8" ? "active" : ""}
+                >
                   <Link to="/lesson8">Lesson 8: Barbecue</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson9" ? "active" : ""}
+                >
                   <Link to="/lesson9">Lesson 9: Kabuki</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson10" ? "active" : ""}
+                >
                   <Link to="/lesson10">Lesson 10: Winter Vacation Plans</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson11" ? "active" : ""}
+                >
                   <Link to="/lesson11">Lesson 11: After the Vacation</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson12" ? "active" : ""}
+                >
                   <Link to="/lesson12">Lesson 12: Feeling Ill</Link>
                 </li>
               </ul>
@@ -133,39 +157,61 @@ function Table() {
             </button>
             {genkiTwoOpen && (
               <ul className="nested">
-                <li>
+                <li
+                  className={location.pathname === "/lesson13" ? "active" : ""}
+                >
                   <Link to="/lesson13">
                     Lesson 13: Looking for a Part-time Job
                   </Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson14" ? "active" : ""}
+                >
                   <Link to="/lesson14">Lesson 14: Valentine's Day</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson15" ? "active" : ""}
+                >
                   <Link to="/lesson15">Lesson 15: A Trip to Nagano</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson16" ? "active" : ""}
+                >
                   <Link to="/lesson16">Lesson 16: Lost and Found</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson17" ? "active" : ""}
+                >
                   <Link to="/lesson17">Lesson 17: Grumble and Gossip</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson18" ? "active" : ""}
+                >
                   <Link to="/lesson18">Lesson 18: John's Part-Time Job</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson19" ? "active" : ""}
+                >
                   <Link to="/lesson19">Lesson 19: Meeting the Boss</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson20" ? "active" : ""}
+                >
                   <Link to="/lesson20">Lesson 20: Mary Goes Shopping</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson21" ? "active" : ""}
+                >
                   <Link to="/lesson21">Lesson 21: Burglar</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson22" ? "active" : ""}
+                >
                   <Link to="/lesson22">Lesson 22: Education in Japan</Link>
                 </li>
-                <li>
+                <li
+                  className={location.pathname === "/lesson23" ? "active" : ""}
+                >
                   <Link to="/lesson23">Lesson 23: Good-bye</Link>
                 </li>
               </ul>
