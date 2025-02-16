26d44fe6b5b851857c208cbe8b9be21ddd794ba9
patch <patchen21@gmail.com>
2025-02-16 14:07:08 -0500

    travel initial greetings

--- src/pages/Travel.js
+++ src/pages/Travel.js
@@ -5,7 +5,128 @@ function Travel() {
     <div>
       <div id="greetings">
         <h2>Greetings</h2>
-        {/* Content for Greetings --> */}
+        <section id="section1">
+          <div>
+            <h3>1. Hello</h3>
+            <p>
+              こんにちは
+              <br />
+              <i>konnichiwa</i>
+            </p>
+          </div>
+          <div>
+            <h3>2. Good Morning</h3>
+            <p>
+              おはようございます
+              <br />
+              <i>ohayou gozaimasu</i>
+            </p>
+          </div>
+          <div>
+            <h3>3. Good Evening</h3>
+            <p>
+              こんばんは
+              <br />
+              <i>konbanwa</i>
+            </p>
+          </div>
+          <div>
+            <h3>4. Goodbye</h3>
+            <p>
+              さようなら
+              <br />
+              <i>sayonara</i>
+            </p>
+          </div>
+          <div>
+            <h3>5. Thank You</h3>
+            <p>
+              ありがとうございます
+              <br />
+              <i>arigatou gozaimasu</i>
+            </p>
+          </div>
+          <div>
+            <h3>6. Excuse Me</h3>
+            <p>
+              すみません
+              <br />
+              <i>sumimasen</i>
+            </p>
+          </div>
+          <div>
+            <h3>7. Yes</h3>
+            <p>
+              はい
+              <br />
+              <i>hai</i>
+            </p>
+          </div>
+          <div>
+            <h3>8. No</h3>
+            <p>
+              いいえ
+              <br />
+              <i>iie</i>
+            </p>
+          </div>
+          <div>
+            <h3>9. Please</h3>
+            <p>
+              お願いします
+              <br />
+              <i>onegai shimasu</i>
+            </p>
+          </div>
+          <div>
+            <h3>10. I'm Sorry</h3>
+            <p>
+              ごめんなさい
+              <br />
+              <i>gomen nasai</i>
+            </p>
+          </div>
+          <div>
+            <h3>11. How Are You?</h3>
+            <p>
+              お元気ですか
+              <br />
+              <i>ogenki desu ka</i>
+            </p>
+          </div>
+          <div>
+            <h3>12. I'm Fine</h3>
+            <p>
+              元気です
+              <br />
+              <i>genki desu</i>
+            </p>
+          </div>
+          <div>
+            <h3>13. What is Your Name?</h3>
+            <p>
+              お名前は何ですか
+              <br />
+              <i>onamae wa nan desu ka</i>
+            </p>
+          </div>
+          <div>
+            <h3>14. My Name is...</h3>
+            <p>
+              私の名前は...です
+              <br />
+              <i>watachi no namae wa ... desu</i>
+            </p>
+          </div>
+          <div>
+            <h3>15. Nice to Meet You</h3>
+            <p>
+              初めまして
+              <br />
+              <i>hajimemashite</i>
+            </p>
+          </div>
+        </section>
       </div>
 
       <div id="airport">
